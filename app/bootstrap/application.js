72a7cb89e52999d274ea827ee257128e952f2175
cyberbird20 <fehichioya@gmail.com>
2014-10-07 05:45:09 +0100

    Update application.js
    Added Authorization field in the res 'Access-Control-Allow-Headers' Headers to allow cross site ajax calls

--- app/bootstrap/application.js
+++ app/bootstrap/application.js
@@ -26,7 +26,7 @@ app.use(multer({
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,Authorization');
   next();
 });
 
