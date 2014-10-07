ddb8d184f2e255ee369e49e64752103c7e289604
Laju Morrison <morrelinko@gmail.com>
2014-10-07 19:49:20 +0100

    Allow cross origin XMLHttpRequest requests

--- app/bootstrap/application.js
+++ app/bootstrap/application.js
@@ -24,6 +24,12 @@ app.use(multer({
   dest: __dirname + '/../storage/tmp'
 }));
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  next();
+});
+
 app.use(morgan({
   immediate: false,
   format: function(tokens, req, res) {
