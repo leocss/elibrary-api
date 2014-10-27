048a095c65ae3c60ad1397373f562862c1b36df8
Laju Morrison <morrelinko@gmail.com>
2014-10-27 04:35:29 +0100

    Implemented resolving same access token for similar clients and users to solve the issue of a user getting logged out of one device when they log in to another

--- app/library/oauth2/grant/password.js
+++ app/library/oauth2/grant/password.js
@@ -3,6 +3,7 @@
  */
 
 var _ = require('lodash'),
+  Promise = require('bluebird'),
   Grant = require('./base'),
   models = require('../../../models'),
   errors = require('../../../errors'),
@@ -18,11 +19,11 @@ var Password = utils.extendObject(function () {
 
 Password.methods.completeFlow = function (req) {
   return this.verifyClient(req).bind(this).then(function () {
+    var self = this;
     var input, session, promise, token, user;
 
     input = _.extend(req.query, req.body);
     token = {
-      access_token: this.generateToken(),
       token_type: TOKEN_TYPE,
       expires_in: 3600 // TODO: change this to 60 days
     };
@@ -53,10 +54,30 @@ Password.methods.completeFlow = function (req) {
       );
     });
 
-    // Done with user validation and everything went well (i hope ---> [No Tests]).
-    return promise.bind(this).tap(function (user) {
-      return this.clearSession(this.client.get('id'), user.get('id'));
-    }).bind(this).then(function (user) {
+    // Done with user validation and everything went well (i hope -- cos -> [No Tests]).
+    return promise.bind(this).then(function (user) {
+      // Check if this client and user already has an active session created.
+      // If it has, the system returns the access token instead of generating a new one.
+      // This fixes issue [logs out a user from a device when the user logs into another device]
+      // Credits to 'Famous Ehichioha' for bringing my attention to this issue. :)
+      return models.ApiSession.findOne({
+        client_id: this.client.get('id'),
+        user_id: user.get('id')
+      }, {require: false});
+    }).then(function (session) {
+      var promises = [];
+      if (session && !session.isExpired()) {
+        // A session does exists... build the token from this instead.
+        token.access_token = session.get('token');
+      } else {
+        // No session exists already... or probably access token expired.
+        token.access_token = self.generateToken();
+      }
+
+      // Just clear any dangling sessions...
+      return this.clearSession(self.client.get('id'), user.get('id'));
+      
+    }).then(function () {
       // Create a (client <--> user) access session...
       return models.ApiSession.create({
         client_id: this.client.get('id'),
@@ -64,13 +85,14 @@ Password.methods.completeFlow = function (req) {
         user_id: user.get('id'),
         life_time: token.expires_in,
         token: token.access_token
-      })
+      });
     }).then(function (session) {
       token.created_at = session.get('created_at');
       token.user_id = user.get('id');
+
       // Flow is complete, return the token
       return token;
-    })
+    });
   })
 };
 
