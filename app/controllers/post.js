aaf99b9d131e0c7ca38dbafabb03344bad700a14
Laju Morrison <morrelinko@gmail.com>
2014-10-27 20:46:59 +0100

    Code updates

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -4,7 +4,7 @@
 
 var _ = require('lodash'),
   knex = require('knex'),
-  Promise  = require('bluebird'),
+  Promise = require('bluebird'),
   utils = require('../utils'),
   errors = require('../errors'),
   models = require('../models');
@@ -18,8 +18,8 @@ module.exports = {
    * @param req
    * @param res
    */
-  getPosts: function(context, req, res) {
-    var model = new models.Post().query(function(query) {
+  getPosts: function (context, req, res) {
+    var model = new models.Post().query(function (query) {
       if (req.query.filter) {
         query.where('title', 'LIKE', '%' + req.query.filter.replace(' ', '%').replace('+', '%') + '%');
       }
@@ -48,20 +48,29 @@ module.exports = {
    * @param req
    * @param res
    */
-  getPost: function(context, req, res) {
+  getPost: function (context, req, res) {
     var includes = context.parseIncludes(['author', 'likes', 'comments', 'comments.user']);
 
     return models.Post.findOne({
       id: req.params.id
     }, {
       withRelated: includes
-    }, function(qb) {
+    }, function (qb) {
       qb.select(
         knex.raw('(SELECT COUNT(id) FROM comments WHERE object = "post" AND object_id = "' + req.params.id + '") AS comments_count'));
       qb.select(
         knex.raw('(SELECT COUNT(id) FROM likes WHERE object = "post" AND object_id = "' + req.params.id + '") AS likes_count'));
       qb.select(
         knex.raw('(SELECT COUNT(id) FROM views WHERE object = "post" AND object_id = "' + req.params.id + '") AS views_count'));
+
+      if (context.user) {
+        qb.select(
+          knex.raw('(' +
+          'SELECT COUNT(likes.id) FROM likes ' +
+          'WHERE object = "post" AND object_id = "' + req.params.id + '" AND user_id = "' + context.user.get('id') + '"' +
+          ') AS context_user_liked')
+        );
+      }
     });
   },
 
@@ -101,13 +110,13 @@ module.exports = {
 
   /**
    * Gets all posts categories
-   * 
+   *
    * @param  {Context} context Request Context
    * @param  {Object} req Http Server Request
    * @param  {Object} res Http Server Response
    * @return {Promise}
    */
-  getCategories: function(context, req, res) {
+  getCategories: function (context, req, res) {
     var promises = [],
       includes = context.parseIncludes(['posts', 'posts.author']);
 
@@ -126,28 +135,28 @@ module.exports = {
 
   /**
    * Creates a new post category.
-   * 
+   *
    * @param  {Context} context Request Context
    * @param  {Object} req Http Server Request
    * @param  {Object} res Http Server Response
    * @return {Promise}
    */
-  createCategory: function(context, req, res) {
+  createCategory: function (context, req, res) {
     var data = _.pick(req.body, ['title', 'description']);
     data.object = 'post';
 
     return models.Category.create(data);
-  },  
+  },
 
   /**
    * Creates a new post / article
-   * 
+   *
    * @param  {Context} context Request Context
    * @param  {Object} req Http Server Request
    * @param  {Object} res Http Server Response
    * @return {Promise}
    */
-  createPost: function(context, req, res) {
+  createPost: function (context, req, res) {
     var data = _.pick(req.body, [
       'title', 'category_id', 'content', 'author_id', 'format'
     ]);
@@ -172,14 +181,14 @@ module.exports = {
    * @param req
    * @param res
    */
-  updatePost: function(context, req, res) {
+  updatePost: function (context, req, res) {
     var data = _.pick(req.body, [
       'title', 'category_id', 'content', 'image', 'is_featured'
     ]);
 
     if (_.has(data, 'title')) {
       data.slug = utils.safeString(data.title);
-    } 
+    }
 
     if (_.has(data, 'content')) {
       // TODO: parse and format data.content.
@@ -189,7 +198,7 @@ module.exports = {
     return models.Post.update(req.params.id, data);
   },
 
-  uploadFeaturedImage: function(context, req, res) {
+  uploadFeaturedImage: function (context, req, res) {
     // TODO: implement
   },
 
@@ -202,11 +211,11 @@ module.exports = {
    * @param request
    * @param response
    */
-  deletePost: function(context, request, response) {
+  deletePost: function (context, request, response) {
     return models.Post.destroy({id: request.params.id});
   },
 
-  getPostLikes: function(context, req, res) {
+  getPostLikes: function (context, req, res) {
     return models.Like.findMany({
       where: {
         object: 'post',
@@ -215,7 +224,7 @@ module.exports = {
     }, {require: false});
   },
 
-  addUserPostLike: function(context, req, res) {
+  addUserPostLike: function (context, req, res) {
     if (context.user === null) {
       // Ensure client access token cannot access this endpoint
       throw new errors.ApiError('Only access token gotten from a user can be used to access this endpoint.');
@@ -232,12 +241,12 @@ module.exports = {
     return models.Like.findOne({
       user_id: data.user_id,
       object_id: data.object_id
-    }).catch(models.Like.NotFoundError, function(err) {
+    }).catch(models.Like.NotFoundError, function (err) {
       return models.Like.create(data);
     });
   },
 
-  removeUserPostLike: function(context, req, res) {
+  removeUserPostLike: function (context, req, res) {
     if (context.user === null) {
       // Ensure client access token cannot access this endpoint
       throw new errors.ApiError('Only access token gotten from a user can be used to access this endpoint.');
@@ -250,7 +259,7 @@ module.exports = {
     }).return({success: true});
   },
 
-  getPostViews: function(context, req, res) {
+  getPostViews: function (context, req, res) {
     return models.View.findMany({
       where: {
         object: 'post',
@@ -259,7 +268,7 @@ module.exports = {
     }, {require: false});
   },
 
-  addPostUserView: function(context, req, res) {
+  addPostUserView: function (context, req, res) {
     if (context.user === null) {
       // Ensure client access token cannot access this endpoint
       throw new errors.ApiError('Only access token gotten from a user can be used to access this endpoint.');
@@ -276,20 +285,20 @@ module.exports = {
     return models.View.findOne({
       user_id: data.user_id,
       object_id: data.object_id
-    }).catch(models.View.NotFoundError, function(err) {
+    }).catch(models.View.NotFoundError, function (err) {
       return models.View.create(data);
     });
   },
 
   /**
    * Creates a new post / article
-   * 
+   *
    * @param  {Context} context Request Context
    * @param  {Object} req Http Server Request
    * @param  {Object} res Http Server Response
    * @return {Promise}
    */
-  getComments: function(context, req, res) {
+  getComments: function (context, req, res) {
     var includes = context.parseIncludes(['user']);
 
     return models.Comment.findMany({
@@ -305,17 +314,17 @@ module.exports = {
 
   /**
    * Creates a new post / article
-   * 
+   *
    * @param  {Context} context Request Context
    * @param  {Object} req Http Server Request
    * @param  {Object} res Http Server Response
    * @return {Promise}
    */
-  createComment: function(context, req, res) {
+  createComment: function (context, req, res) {
     var required = ['content', 'user_id', 'post_id'];
     var data = _.pick(req.body, required);
-    
-    required.forEach(function(item) {
+
+    required.forEach(function (item) {
       if (!_.has(data, item)) {
         throw new errors.MissingParamError([item]);
       }
