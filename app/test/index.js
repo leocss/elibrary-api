49cadb58907dfafb2961e7b05eb9d02cdaf8b1f2
Laju Morrison <morrelinko@gmail.com>
2014-11-17 12:18:35 +0100

    Code updates

---
+++ app/test/index.js
@@ -0,0 +1,17 @@
+var chai = require('chai');
+
+describe('Authentication', function() {
+    it('Should return an access token if details are valid', function() {
+
+    });
+});
+
+describe('Resources', function() {
+    it ('Should return error response if no access_token is specified.', function() {
+
+    });
+
+    it ('Should return a book resource', function() {
+
+    });
+});
