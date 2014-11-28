4c65da4f3ec07a6685c0746d76abad243f73b7a7
Laju Morrison <morrelinko@gmail.com>
2014-11-28 13:14:33 +0100

    Removed spindrift

--- app/controllers/book.js
+++ app/controllers/book.js
@@ -7,7 +7,6 @@ var fs = require('fs');
 var path = require('path'),
   _ = require('lodash'),
   moment = require('moment'),
-  spindrift = require('spindrift'),
   Promise = require('bluebird'),
   knex = require('knex'),
   fse = Promise.promisifyAll(require('fs-extra')),
